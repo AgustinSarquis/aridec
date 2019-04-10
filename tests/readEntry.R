44f976f14cc3a401205a4a36fe3f65a56be8c87d
Agustín <agusarquis@agro.uba.ar>
2019-04-10 16:54:12 -0300

    Day2018 started

--- tests/readEntry.R
+++ tests/readEntry.R
@@ -8,6 +8,6 @@ metadataAustin2006=yaml.load_file(input = "~/Repos/aridec/data/Austin2006/metada
 metadataAustin2006 # If the previous line works without error you can now see the entry as an R list
 
 # Test if you can read the data
-dataAustin2006=read.csv("~/Repos/aridec/data/Austin2006/data.csv")
+dataAustin2006=read.csv("~/aridec/data/Austin2006/data.csv")
 plot(dataAustin2006) #If you can see a plot, then R can read the data
 
