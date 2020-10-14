f9014414c863f5806050c59bf9769296bb447b12
AgustinSarquis <agusarquis@agro.uba.ar>
2020-10-14 11:39:41 -0300

    created a lignin fuction

---
+++ Rpkg/R/lignin.R
@@ -0,0 +1,12 @@
+#' Creates a data frame with the lignin content in litter samples
+#'
+#' @param database A list with the aridec structure
+#' @return A data frame with the lignin content (%) of the litter samples from the database
+#' @export
+#' @examples
+#' lignin=lignin(database=aridec)
+lignin= function(database){
+  mean=lapply(database, FUN=function(x){x$initConditions$lignin})
+  return(data.frame(lignin=unlist(mean) 
+  ))
+}
