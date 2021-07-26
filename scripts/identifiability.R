760b72d2872711e4223ab86c38eeaf44c78d2a40
AgustinSarquis <agusarquis@agro.uba.ar>
2021-07-26 14:41:43 -0300

    Yang2016 added

--- scripts/identifiability.R
+++ scripts/identifiability.R
@@ -6,13 +6,13 @@ library(dplyr)
 
 # load single entry
 db=loadEntries()
-entry=db[[Bernaschini2019]]
+entry=db$Henry2008
 Ct=entry$timeSeries[,c(1,2)] 
 colnames(Ct)=c("time", "Ct")
 
-Ct=mutate(Ct, time=time*7) # semanas a dias
-Ct=mutate(Ct, time=time*30) # transformo meses a dias
-Ct=mutate(Ct, time=time*365) # anos a dias
+Ct=mutate(Ct, time=time*7) # transform weeks to days
+Ct=mutate(Ct, time=time*30) # transform months to days
+Ct=mutate(Ct, time=time*365) # transform years to days
 
 # 2 pool parallel model 
 inipars=c(1, 0.5, 0.5)
@@ -201,6 +201,8 @@ pairs(MCMC, nsample = 1000)
 ###########################################################################################
 # graficos
 library(ggplot2)
+library(cowplot)
+library(gridExtra)
 df=read.csv("~/coldf.csv")
 summary(df)
 df$X1=as.factor(df$X1)
@@ -212,45 +214,58 @@ df$X6=as.factor(df$X6)
 df$X7=as.factor(df$X7)
 df$N=as.factor(df$N)
 df$length=as.factor(df$length)
-# modifique el dataframe para agrupar valores >8
-gtwopp=ggplot(subset(df, model %in% "twopp"), aes(N, log.col, color=length))
-gtwopp+geom_point(position="jitter", size=3) +
- geom_abline(intercept= log10(20), slope=0) + ylim(0,3.5) + theme_bw() +
-  labs(y="log(Collinearity)", x=("Number of parameters in combination")) + 
-  labs(colour="Number of time points") +   ggtitle("Two Pool Parallel Model") +
-  geom_vline(xintercept = c("2", "3"), linetype=2)
 
-gtwops=ggplot(subset(df, model %in% "twops"), aes(N, log.col, color=length))
-gtwops+geom_point(position="jitter", size=3) +
+arcoiris=c("red", "orange", "yellow", "green", "blue", "purple")
+names(arcoiris)=c("3", "4", "5", "6", "7", ">8")
+arcoiris
+
+gtwopp=ggplot(subset(df, model %in% "twopp"), aes(N, log.col, color=length.1))
+plot1=gtwopp+geom_point(position="jitter", size=3, show.legend = FALSE) +
+ geom_abline(intercept= log10(20), slope=0) + ylim(0,NA) + theme_bw() +
+  labs(y="log ??", x=("Number of parameters in combination")) +
+  ggtitle("Two Pool Parallel Model") +  geom_vline(xintercept = c("2", "3"), linetype=2) +
+  scale_colour_manual(name= "length.1", values=arcoiris)
+
+gtwops=ggplot(subset(df, model %in% "twops"), aes(N, log.col, color=length.1))
+plot2=gtwops+geom_point(position="jitter", size=3, show.legend = FALSE) +
   geom_abline(intercept= log10(20), slope=0) + #ylim(0,3.5) + 
   theme_bw() +
-  labs(y="log(Collinearity)", x=("Number of parameters in combination")) + 
-  labs(colour="Number of time points") +   ggtitle("Two Pool Series Model") +
-  geom_vline(xintercept = c("2", "3", "4"), linetype=2)
+  labs(y="log ??", x=("Number of parameters in combination")) + 
+  ggtitle("Two Pool Series Model") +  geom_vline(xintercept = c("2", "3", "4"), linetype=2) +
+  scale_colour_manual(name= "length.1", values=arcoiris)
 
-gtwopf=ggplot(subset(df, model %in% "twopf"), aes(N, log.col, color=length))
-gtwopf+geom_point(position="jitter", size=3) +
-  geom_abline(intercept= log10(20), slope=0) + #ylim(0,3.5) + 
-  theme_bw() +
-  labs(y="log(Collinearity)", x=("Number of parameters in combination")) + 
-  labs(colour="Number of time points") +   ggtitle("Two Pool Model with Feedback") +
-  geom_vline(xintercept = c("2", "3", "4","5"), linetype=2)
+gtwopf=ggplot(subset(df, model %in% "twopf"), aes(N, log.col, color=length.1))
+plot3=gtwopf+geom_point(position="jitter", size=3, show.legend = FALSE) +
+  geom_abline(intercept= log10(20), slope=0) + ylim(0,8) + 
+  theme_bw() + labs(y="log ??", x=("Number of parameters in combination")) +
+  ggtitle("Two Pool Model with Feedback") +  geom_vline(xintercept = c("2", "3", "4","5"), linetype=2) +
+  scale_colour_manual(name= "length.1", values=arcoiris)
 
-gthreepp=ggplot(subset(df, model %in% "threepp"), aes(N, log.col, color=length))
-gthreepp+geom_point(position="jitter", size=3) +
+gthreepp=ggplot(subset(df, model %in% "threepp"), aes(N, log.col, color=length.1))
+plot4=gthreepp+geom_point(position="jitter", size=3, show.legend = FALSE) +
   geom_abline(intercept= log10(20), slope=0) +# ylim(0,10) + 
-  theme_bw() +
-  labs(y="log(Collinearity)", x=("Number of parameters in combination")) + 
-  labs(colour="Number of time points") +   ggtitle("Three Pool Parallel Model") +
-  geom_vline(xintercept = c("2", "3", "4","5"), linetype=2)
-# use el dataframe sin modiificar
-gthreeps=ggplot(subset(df, model %in% "threeps"), aes(N, log.col, color=length))
-gthreeps+geom_point(position="jitter", size=3) +
+  theme_bw() +  labs(y="log ??", x=("Number of parameters in combination")) + 
+  ggtitle("Three Pool Parallel Model") +  geom_vline(xintercept = c("2", "3", "4","5"), linetype=2) +
+  scale_colour_manual(name= "length.1", values=arcoiris)
+
+gthreeps=ggplot(subset(df, model %in% "threeps"), aes(N, log.col, color=length.1))
+plot5=gthreeps+geom_point(position="jitter", size=3, show.legend = FALSE) +
   geom_abline(intercept= log10(20), slope=0) +# ylim(0,10) + 
-  theme_bw() +
-  labs(y="log(Collinearity)", x=("Number of parameters in combination")) + 
-  labs(colour="Number of time points") +   ggtitle("Three Pool Series Model") +
-  geom_vline(xintercept = c("2", "3", "4","5","6","7"), linetype=2)
+  theme_bw() +  labs(y="log ??", x=("Number of parameters in combination")) + 
+  ggtitle("Three Pool Series Model") +  geom_vline(xintercept = c("2", "3", "4","5","6","7"), linetype=2) +
+  scale_colour_manual(name= "length.1", values=arcoiris)
+
+# I only made this plot to extract the legend
+plotL=gtwopp+geom_point(position="jitter", size=3) +
+  geom_abline(intercept= log10(20), slope=0) + ylim(0,NA) + theme_bw() +
+  labs(y="log ??", x=("Number of parameters in combination")) +
+  ggtitle("Two Pool Parallel Model") +  geom_vline(xintercept = c("2", "3"), linetype=2) +
+  scale_colour_manual(name= "Number of time points", values=arcoiris) +
+  theme(legend.position="bottom", legend.text(font="bold")) 
+
+legend <- cowplot::get_legend(plotL)
+
+grid.arrange(plot1, plot2, plot3, plot4, plot5, legend, ncol=2)
 
 ###################################################################################################################
 # porcentaje de entradas identificables por modelo
@@ -258,41 +273,115 @@ twoppsub = subset(df, model == "twopp", select = c("X1", "X2", "X3", "N", "log.c
 all_pars_2pp= filter(twoppsub, N %in% "3")
 identifiable=c(all_pars_2pp$log.col <= log10(20))
 all_pars_2pp=cbind(all_pars_2pp,identifiable)
-count(all_pars_2pp$identifiable)
+length(all_pars_2pp$identifiable[all_pars_2pp$identifiable==T])
+18/29
+rest_2pp=filter(twoppsub, N %in% "2")
+identifiable=c(rest_2pp$log.col <= log10(20))
+rest_2pp=cbind(rest_2pp,identifiable)
+length(rest_2pp$identifiable[rest_2pp$identifiable==T])
+86/90
 ks_2pp=filter(twoppsub, X1 %in% "1", X2 %in% "1", X3 %in% "0")
 identifiable=c(ks_2pp$log.col <= log10(20))
 ks_2pp=cbind(ks_2pp,identifiable)
-count(ks_2pp$identifiable)
+length(ks_2pp$identifiable[ks_2pp$identifiable==T])
 
 twopssub = subset(df, model == "twops", select = c("X1", "X2", "X3", "X4","N", "log.col")) 
 all_2ps=filter(twopssub, N%in% "4")
 identifiable=c(all_2ps$log.col <= log10(20))
 all_2ps=cbind(all_2ps,identifiable)
-count(all_2ps$identifiable)/20
+length(all_2ps$identifiable[all_2ps$identifiable==T])
+12/25
+rest2_2ps=filter(twopssub, N%in% "2")
+identifiable=c(rest2_2ps$log.col <= log10(20))
+rest2_2ps=cbind(rest2_2ps,identifiable)
+length(rest2_2ps$identifiable[rest2_2ps$identifiable==T])
+142/150
+rest3_2ps=filter(twopssub, N%in% "3")
+identifiable=c(rest3_2ps$log.col <= log10(20))
+rest3_2ps=cbind(rest3_2ps,identifiable)
+length(rest3_2ps$identifiable[rest3_2ps$identifiable==T])
+69/101
 ks_a_2ps=filter(twopssub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "0")
 identifiable=c(ks_a_2ps$log.col <= log10(20))
 ks_a_2ps=cbind(ks_a_2ps,identifiable)
-count(ks_a_2ps$identifiable)/20
+length(ks_a_2ps$identifiable[ks_a_2ps$identifiable==T])
+22/26
 
 twopfsub = subset (df, model == "twopf", select = c("X1", "X2", "X3", "X4", "X5","N", "log.col"))
 all_2pf=filter(twopfsub, N%in% "5")
 all_2pf=cbind(all_2pf,identifiable=c(all_2pf$log.col <= log10(20)))
-count(all_2pf$identifiable)
+length(all_2pf$identifiable[all_2pf$identifiable==T])
+rest2_2pf=filter(twopfsub, N%in% "2")
+identifiable=c(rest2_2pf$log.col <= log10(20))
+rest2_2pf=cbind(rest2_2pf,identifiable)
+length(rest2_2pf$identifiable[rest2_2pf$identifiable==T])
+86/150
+rest3_2pf=filter(twopfsub, N%in% "3")
+identifiable=c(rest3_2pf$log.col <= log10(20))
+rest3_2pf=cbind(rest3_2pf,identifiable)
+length(rest3_2pf$identifiable[rest3_2pf$identifiable==T])
+7/150
+rest4_2pf=filter(twopfsub, N%in% "4")
+identifiable=c(rest4_2pf$log.col <= log10(20))
+rest4_2pf=cbind(rest4_2pf,identifiable)
+length(rest4_2pf$identifiable[rest4_2pf$identifiable==T])
+0
 ks_as_2pf=filter(twopfsub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "1", X5 %in% "0")
 ks_as_2pf=cbind(ks_as_2pf,identifiable=c(ks_as_2pf$log.col <= log10(20)))
 
 threeppsub =subset(df, model == "threepp", select = c("X1", "X2", "X3", "X4", "X5","N", "log.col"))
+rest4_3pp=filter(threeppsub, N%in% "4")
+identifiable=c(rest4_3pp$log.col <= log10(20))
+rest4_3pp=cbind(rest4_3pp,identifiable)
+length(rest4_3pp$identifiable[rest4_3pp$identifiable==T])
+26/75
+rest3_3pp=filter(threeppsub, N%in% "3")
+identifiable=c(rest3_3pp$log.col <= log10(20))
+rest3_3pp=cbind(rest3_3pp,identifiable)
+length(rest3_3pp$identifiable[rest3_3pp$identifiable==T])
+102/150
+rest2_3pp=filter(threeppsub, N%in% "2")
+identifiable=c(rest2_3pp$log.col <= log10(20))
+rest2_3pp=cbind(rest2_3pp,identifiable)
+length(rest2_3pp$identifiable[rest2_3pp$identifiable==T])
+134/150
 ks_c1_3pp=filter(threeppsub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "1", X5 %in% "0")
 ks_c1_3pp=cbind(ks_c1_3pp, identifiable=c(ks_c1_3pp$log.col <= log10(20)))
-count(ks_c1_3pp$identifiable)
 ks_c2_3pp=filter(threeppsub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "0", X5 %in% "1")
 ks_c2_3pp=cbind(ks_c2_3pp, identifiable=c(ks_c2_3pp$log.col <= log10(20)))
-count(ks_c2_3pp$identifiable)
 ks_3pp=filter(threeppsub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "0", X5 %in% "0")
 ks_3pp=cbind(ks_3pp, identifiable=c(ks_3pp$log.col <= log10(20)))
 count(ks_3pp$identifiable)
 
 threepssub= subset(df, model =="threeps", select= c("X1", "X2", "X3", "X4", "X5","X6","X7","N", "log.col"))
+rest6_3ps=filter(threepssub, N%in% "6")
+identifiable=c(rest6_3ps$log.col <= log10(20))
+rest6_3ps=cbind(rest6_3ps,identifiable)
+length(rest6_3ps$identifiable[rest6_3ps$identifiable==T])
+2/35
+rest5_3ps=filter(threepssub, N%in% "5")
+identifiable=c(rest5_3ps$log.col <= log10(20))
+rest5_3ps=cbind(rest5_3ps,identifiable)
+length(rest5_3ps$identifiable[rest5_3ps$identifiable==T])
+11/105
+rest4_3ps=filter(threepssub, N%in% "4")
+identifiable=c(rest4_3ps$log.col <= log10(20))
+rest4_3ps=cbind(rest4_3ps,identifiable)
+length(rest4_3ps$identifiable[rest4_3ps$identifiable==T])
+49/175
+rest3_3ps=filter(threepssub, N%in% "3")
+identifiable=c(rest3_3ps$log.col <= log10(20))
+rest3_3ps=cbind(rest3_3ps,identifiable)
+length(rest3_3ps$identifiable[rest3_3ps$identifiable==T])
+96/175
+rest2_3ps=filter(threepssub, N%in% "2")
+identifiable=c(rest2_3ps$log.col <= log10(20))
+rest2_3ps=cbind(rest2_3ps,identifiable)
+length(rest2_3ps$identifiable[rest2_3ps$identifiable==T])
+89/105
+ks_as_c1_3ps=filter(threepssub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "1", X5 %in% "1", X6 %in% "1", X7 %in% "0")
+ks_as_c1_3ps=cbind(ks_as_c1_3ps,identifiable=c(ks_as_c1_3ps$log.col <= log10(20)))
+ks_as_c2_3ps=filter(threepssub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "1", X5 %in% "1", X6 %in% "0", X7 %in% "1")
+ks_as_c2_3ps=cbind(ks_as_c2_3ps,identifiable=c(ks_as_c2_3ps$log.col <= log10(20)))
 ks_as_3ps=filter(threepssub, X1 %in% "1", X2 %in% "1", X3 %in% "1", X4 %in% "1", X5 %in% "1", X6 %in% "0", X7 %in% "0")
 ks_as_3ps=cbind(ks_as_3ps,identifiable=c(ks_as_3ps$log.col <= log10(20)))
-count(ks_as_3ps$identifiable)                
